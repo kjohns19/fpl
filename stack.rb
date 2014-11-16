92530cf641632c961ffdbd7a4958df8d35540f7e
chrisbeard <christopher.e.beard@gmail.com>
2014-11-15 19:10:06 -0500

    fix tabs to spaces, again

--- stack.rb
+++ stack.rb
@@ -6,18 +6,18 @@ class Stack
         @stack = []
     end
 
-	def push(val)
-		if val.is_a? Operator
-			val.execute(self)
-		else
+    def push(val)
+        if val.is_a? Operator
+            val.execute(self)
+        else
             if val.path
                 val.path = Utils.absolute_path(val.path)
             else
                 val.path = Utils.generate_temp_path
             end
-			@stack << val
-		end
-	end
+            @stack << val
+        end
+    end
 
     def pop
         var = @stack.pop
